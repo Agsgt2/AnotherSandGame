821d395970445064c6ea9809559390e9a2345cce
Agsgt2 <angelthefourth20242026@gmail.com>
2025-01-04 21:32:59 -0600

    Update CustomElements.js

--- CustomElements.js
+++ CustomElements.js
@@ -1,4 +1,47 @@
 SpaceTode`
+element normalWater{
+  @ =>_
+  _   @
+
+  any(x){
+    @_=>_@
+  }
+}
+
+element forWater{
+  @ =>_
+  _   @
+
+  for(x){
+    @_=>_@
+  }
+}
+
+element slideWater{
+  @ =>_
+  _   @
+
+  for(x){
+    @_=>_@
+
+    @ =>_
+     _   @
+  }
+}
+
+element slide2Water{
+  @ =>_
+  _   @
+
+  for(x){
+    @_=>_@
+
+    @__=>__@
+
+    @ =>_
+     _   @
+  }
+}
 
 
 `
